f0b7740b49bb5b9383cf950c01e127a7a65efb70
anas <muhammedanas01@gmail.com>
2024-11-14 01:30:09 +0530

    using Zustand a centralized state managment it configured.

---
+++ Frontend/src/store/auth.js
@@ -0,0 +1,34 @@
+//In the context of Zustand (a state management library for JavaScript), the store refers to a central place where
+//...the state of your application is stored and managed. This store allows different parts of your application to share and update state
+import {create} from 'zustand'
+import {mountStoreDevtool} from 'simple-zustand-devtool'
+
+//This setup creates a simple state management structure where allUserData can be used to store user-related information,..
+//...and loading can be used to track whether a process is in progress.
+//set and get are utility functions provided by Zustand to interact with the store's state.
+const useAuthStore = create((set, get)=>({
+    allUserData: null,
+    loading: false,
+
+    //The snippet user: () => { } is an example of a property within an object where the value is an arrow function.
+    // eg  console.log(user) Outputs: { user_id: 1, username: 'john_doe' }
+    user: () =>({
+    user_id : get().allUserData?.user_id || null,
+    username: get().allUserData?.username || null,
+    }),
+
+    //set, is a function provided by the Zustand store to update the state.
+    //set takes an object as an argument. Here, it’s { allUserData: user }
+    //This object tells Zustand to update the state where allUserData will be set to whatever value user(parameter) holds.
+    setUser:  (user) => set({ allUserData: user }),
+    setLoading: (loading) => set({loading}),
+    setLoggedIn: () => get().allUserData !== null,
+
+}))
+
+if(import.meta.env.DEV){
+    mountStoreDevtool('Store', 'useAuthStore')
+}
+
+export {useAuthStore}
+
