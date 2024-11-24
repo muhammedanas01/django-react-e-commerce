2b349256ead848b82a8d3fcfacfecdee399f4ce7
anas <muhammedanas01@gmail.com>
2024-11-24 16:37:15 +0530

    models created for vendor,product and category in store

--- Backend/store/models.py
+++ Backend/store/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from vendor.models import Vendor
+from userauths.models import User, Profile
+
+from shortuuid.django_fields import ShortUUIDField
+
+
+STATUS = (
+    ("draft","Draft"),
+    ("disabled","Disabled"),
+    ("in_review","In Review"),
+    ("published","Published"),
+
+)
+
+class Category(models.Model):
+    title =  models.CharField(max_length=100)
+    image = models.FileField(upload_to="category", default="category.jpg", null=True,)
+    active = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = "Category"
+        ordering = ['-title']   
+
+    def __str__(self):
+        return self.title
+    
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="category", default="product.jpg", null=True,)
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)# under which category this product comes
+    price = models.DecimalField(decimal_places=2, max_digits=12, default=0.00) # for price
+    old_price = models.DecimalField(decimal_places=2, max_digits=12, default=0.00)
+    shipping_amount = models.DecimalField(decimal_places=2, max_digits=12, default=0.00)
+
+    stock_quantity = models.PositiveIntegerField(default=1) # vendor must have atleast one item in quantity
+    in_stock = models.BooleanField(default=True)
+
+    status = models.CharField(max_length=100, choices=STATUS, default="published")
+    featured = models.BooleanField(default=False)
+    views = models.PositiveIntegerField(default=0)
+    rating  = models.PositiveIntegerField(default=0)
+
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE) #many to-one relationship. one vendor can have many products 
+
+    product_id  = ShortUUIDField(unique=True, length=10, prefix="PROD", alphabet="1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    slug = models.SlugField(unique=True)
+
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+        ordering = ['-title']   
+
+    def __str__(self):
+        return str(self.title)
+
+    #over rides default save method
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug == slugify(self.title)
+
+        super(Product, self).save( *args, **kwargs)# ensures that the model instance is saved
+
+
+
+
+
+
+
--- Backend/vendor/models.py
+++ Backend/vendor/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.text import slugify
 
+from userauths.models import User, Profile
 # Create your models here.
+
+class Vendor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # one user one vendor 
+    image = models.FileField(upload_to="vendor", blank=True, default="vendor.jpg"),# shop avatar or image
+    name = models.CharField(max_length=200, help_text="Shop Name", null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    mobile = models.CharField(max_length=200, help_text="shop mobile number", null=False, blank=False)
+    active = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, max_length=500)
+
+    class Meta:
+        verbose_name_plural = "vendors"
+        ordering = ['-date']
+
+    def __str__(self):
+        return str(self.name)
+    #over rides default save method
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug == slugify(self.name)
+
+        super(Vendor, self).save( *args, **kwargs)# ensures that the model instance is saved
+
+    
